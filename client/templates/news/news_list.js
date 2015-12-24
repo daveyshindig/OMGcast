0a1a60c55d08fe3fc525671114820a4cec9f1a0c
daveyshindig <davey.wilkie@gmail.com>
2015-12-23 16:51:12 -1000

    Wip

--- client/templates/news/news_list.js
+++ client/templates/news/news_list.js
@@ -1,9 +1,18 @@
 Template.newsList.onCreated(function () {
   var self = this;
+
+  Session.set('chapp-docid', '808mix');
+  Session.set('chapp-historysince', new Date(0));
+  if (Meteor.user() != null) {
+    Session.set('chapp-username', Meteor.user().username);
+  }
+
   self.autorun(function() {
     var limit = FlowRouter.getParam('limit') || 8;
     self.subscribe('posts', {sort: {submitted: -1}, limit: limit});
-  })
+    self.subscribe('chapps', Session.get('chapp-docid'),
+                             Session.get('chapp-historysince'));
+  });
 });
 
 Template.newsList.onRendered(function () {
